1879fb4effb8cd51ef05ba8f77cdbc544789e3fd
Joshua Stafford <Josh@Joshuas-MacBook-Pro.local>
2017-11-22 20:19:50 -0600

    Initial push to heroku 3

--- Vana/settings.py
+++ Vana/settings.py
@@ -133,3 +133,10 @@ STATICFILES_DIRS = (
 
 # Extra places for collectstatic to find static files.
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
