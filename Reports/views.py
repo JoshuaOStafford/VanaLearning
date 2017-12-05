f2fe0290380d986adc85077a1e53ca4b42017f57
Joshua Stafford <Josh@Joshuas-MacBook-Pro.local>
2017-12-04 23:25:47 -0600

    Making timezone change

--- Reports/views.py
+++ Reports/views.py
@@ -119,6 +119,7 @@ def teacher_submissions_view(request):
     if not teacher == Teacher.objects.get(username='lhorich'):
         return redirect('/home')
     date = datetime.now(timezone.utc)
+    date_str = str(date)
     a6 = DRC.objects.filter(teacher=Teacher.objects.get(username='dbleiberg'), date=date).count()
     b6 = DRC.objects.filter(teacher=Teacher.objects.get(username='mdemers'), date=date).count()
     c6 = DRC.objects.filter(teacher=Teacher.objects.get(username='cwest'), date=date).count()
@@ -136,6 +137,6 @@ def teacher_submissions_view(request):
 
     return render(request, 'teacher_submissions.html', {'user': teacher, '6a': a6, '6b': b6, '6c': c6, '6d': d6,
                                                         '7a': a7, '7b': b7, '7c': c7, '7d': d7,'8a': a8, '8b': b8,
-                                                        '8c': c8, '8d': d8})
+                                                        '8c': c8, '8d': d8, 'date': date_str})
 
 
