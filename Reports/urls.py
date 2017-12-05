761ddf1dfad75cf8a93f520dc42ad0ecae2ae06c
Joshua Stafford <Josh@Joshuas-MacBook-Pro.local>
2017-12-04 22:52:18 -0600

    Testing teacher submission view

--- Reports/urls.py
+++ Reports/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^StudentHistory/([a-zA-z0-9_-]{3,16})', views.student_history_view, name='student history'),
     url(r'^ProgressGraph/([a-zA-z0-9_-]{3,16})', views.progress_graph_view, name='progress graph'),
     url(r'^WeeklyReports/([a-zA-z0-9_-]{3,16})', views.weekly_reports_view, name='view weekly reports'),
-    url(r'TeacherSubmissions/', views.teacher_submissions_view, name='view teacher submission stats')
+    url(r'TeacherSubmissions', views.teacher_submissions_view, name='view teacher submission stats')
 ]
 
