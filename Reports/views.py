f675963c0dc7143d7df18e6dbfd5f83fe78e3bb4
Joshua Stafford <Josh@Joshuas-MacBook-Pro.local>
2017-12-06 16:14:56 -0600

    Changes to Lele's view (only her 2 students shown in Logging and Past Submissions

--- Reports/views.py
+++ Reports/views.py
@@ -36,6 +36,8 @@ def log_drc_view(request):
     if teacher is None:
         return redirect('/home')
     students = teacher.student_set.all()
+    if teacher.username == 'lhorich':
+        students = Student.objects.filter(id>4)
     remaining_students = []
     for student in students:
         if not DRC.objects.filter(student=student, teacher=teacher, date=date).exists():
