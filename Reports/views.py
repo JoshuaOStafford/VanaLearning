9a257663ad110660e9d8b50581674ca397a370c7
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-01-03 05:56:14 -0600

    Working on ordering drcs by date

--- Reports/views.py
+++ Reports/views.py
@@ -96,7 +96,8 @@ def student_history_view(request, student_username):
     if student not in teacher.student_set.all():
         return redirect('/home')
     master_drcs = MasterDRC.objects.filter(student=student)
-    master_drcs = master_drcs.order_by(Coalesce('date').desc())
+    master_drcs = master_drcs.order_by('date')
+    master_drcs.reverse()
     return render(request, 'student_history.html', {'user': teacher, 'student': student, 'Master_DRCs': master_drcs})
 
 
