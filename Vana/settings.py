77f97d2845671ec09f5f77b3b872a591695d9db1
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-01-03 07:06:37 -0600

    Site live

--- Vana/settings.py
+++ Vana/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jj5809ns18d1j!#vpwqn(on^g2vf*c9_8uuew7d5id%qkp&!2j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
